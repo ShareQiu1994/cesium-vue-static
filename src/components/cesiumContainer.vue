c5dff71ff91bb1caf157289cac74e8e73c0734f8
ShareQiu1994 <421419567@qq.com>
2018-11-09 09:31:01 +0800

    2018/11/09

---
+++ src/components/cesiumContainer.vue
@@ -0,0 +1,15 @@
+<template>
+   <div id="cesiumContainer">
+   </div>
+</template>
+<script>
+export default {
+  name: 'cesiumContainer',
+  mounted(){
+     var viewer = new Cesium.Viewer('cesiumContainer');
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
