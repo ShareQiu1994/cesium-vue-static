672291cd1ea16620cc3e2543a33fa93e452dfc27
ShareQiu1994 <421419567@qq.com>
2018-11-09 21:40:30 +0800

    2018/11/09

--- README.md
+++ README.md
@@ -1,21 +1,38 @@
-# cesium-vue-static
+# cesium-vue-static(基于vuecli2.x)
 
-> A Vue.js project
+> 一个整合了Cesium的VueCli2.x的脚手架工具[静态资源版]
+
+1.项目为VueCli+Cesium静态资源版, 集成了cesium罗盘插件与cesium量算插件。
+
+2.根据您的项目需要，可以配置自己需要的模块，如Ui模块(ElementUi,museUi)等。
+
+3.建议开发者将写好的Cesium功能封装成一个独立的模块，而不是将cesium的属性赋值给Vue的data对象中(会造成一些性能问题)。 
+
+为什么使用静态资源版？
+优点：一些第三方cesium插件并没有提供NPM包,以模块化的方式引入cesium没办法直接使用这些第三方插件包,通过静态资源的方式可以解决此问题。
+缺点：没办法以模块的方式引入cesium,也就意味着无法按需加载。 
+
+静态资源版在开发方式有何不同？
+在开发方法上并没有与模块化版本(cesium-vue)没有其他区别,语法与cesium的使用方式一致,包括打包方式。
 
 ## Build Setup
 
 ``` bash
-# install dependencies
+# 安装依赖
 npm install
 
-# serve with hot reload at localhost:8080
+# 运行开发环境
 npm run dev
 
-# build for production with minification
+# 编译
 npm run build
 
-# build for production and view the bundle analyzer report
+# 编译分析报告
 npm run build --report
+
+# 打开浏览器查看运行结果
+localhost:8080
 ```
 
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+# 浏览器兼容报告
+![Image text](https://raw.githubusercontent.com/ShareQiu1994/img-folder/master/webGL.png) 
--- src/components/cesiumContainer.vue
+++ src/components/cesiumContainer.vue
@@ -1,12 +1,79 @@
 <template>
    <div id="cesiumContainer">
+
+
+         <div style="z-index:9999;position: absolute;top: 20px;left: 20px; color:#fff;">
+    <input class="draw" type="radio" name="draw" value="0" checked="checked">贴地形
+    <input class="draw" type="radio" name="draw" value="1">空间
+</div>
+<div style="z-index:9999;position: absolute;top: 50px;left: 20px;">
+    <button id="point">画点</button>
+    <button id="line">画线</button>
+    <button id="polygon">画面</button>
+    <button id="elevation">量高</button>
+    <button id="mpoint">m画点</button>
+    <button id="mline">m画线</button>
+    <button id="mpolygon">m画面</button>
+    <button id="melevation">m量高</button>
+    <button id="remove">移除</button>
+</div>
+
+
    </div>
 </template>
 <script>
 export default {
   name: 'cesiumContainer',
   mounted(){
-     var viewer = new Cesium.Viewer('cesiumContainer');
+      var viewer = new Cesium.Viewer('cesiumContainer');
+       viewer.extend(Cesium.viewerCesiumNavigationMixin, {});
+      
+       const tool =  new Cesium.DrawTool({
+	        viewer:viewer,
+	        isMeasure:true,  // 是否开启测量模式
+	        isClampGround:true,  // 是否开启贴地模式
+	        // lineWidth:1.0    // 设置线宽
+       });
+      
+      	 document.getElementById('point').onclick = function() {
+		        tool.startPoint()  // 画点
+		    };
+		    document.getElementById('line').onclick = function() {
+		        tool.startPolyline()  // 画线
+		    };
+		    document.getElementById('polygon').onclick = function() {
+		        tool.startPolygon();  // 画面
+		    };
+		    document.getElementById('elevation').onclick = function() {
+		        tool.startElevation();  // 量高
+		    };
+		    document.getElementById('mpoint').onclick = function() {
+		        tool.startModelPoint() // 模型画点
+		    };
+		    document.getElementById('mline').onclick = function() {
+		        tool.startModelPolyline()  // 模型画线
+		    };
+		    document.getElementById('mpolygon').onclick = function() {
+		        tool.startModelPolygon();  // 模型画面
+		    };
+		    document.getElementById('melevation').onclick = function() {
+		        tool.startModelElevation();  // 模型量高
+		    };
+		    document.getElementById('remove').onclick = function() {
+		        tool.destory()
+		    };
+
+		   const radios = document.getElementsByName('draw');
+		   for(let item of radios){
+		        item.onclick = function() {
+		            if(item.checked && item.value == 0){
+		                tool.setIsClampGround(true)  // 设置是否开启贴地模式
+		            }else{
+		                tool.setIsClampGround(false)
+		            }
+		        }
+        }
+        
   }
 }
 </script>
